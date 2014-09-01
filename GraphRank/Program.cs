6cdc54ff9c74769e20bd5769d93d56ed16d8039e
Me <quandary82@hailmail.net>
2014-09-01 17:02:37 +0200

    References

--- GraphRank/Program.cs
+++ GraphRank/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
+
 namespace GraphRank
 {
+
 
     // https://github.com/gkatsev/pagerank/blob/master/pagerank.py
     static class Program
@@ -63,6 +66,23 @@ namespace GraphRank
 
 
             float[] web4= {3,3,3};
+
+
+
+            // https://www.eff.org/https-everywhere
+            // https://tools.ietf.org/html/rfc6797
+            // https://www.eff.org/deeplinks/2014/04/why-web-needs-perfect-forward-secrecy
+            // http://stackoverflow.com/questions/20268954/page-rank-matlab
+            // http://www.math.iit.edu/~fass/matlab/pagerank.m
+            // http://www.math.cornell.edu/~mec/Winter2009/RalucaRemus/Lecture3/lecture3.html
+            // http://michaelnielsen.org/blog/using-your-laptop-to-compute-pagerank-for-millions-of-webpages/
+            // http://langvillea.people.cofc.edu/PRDataCode/index.html
+
+
+
+
+
+
 
 
             //RankingAlgorithms.PageRank.Normalize(web4);
